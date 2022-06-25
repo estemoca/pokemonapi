472698049782ea8829892fdc65595311f448d34e
esteban <estemoca@gmail.com>
2022-06-24 21:43:56 -0500

    envio proyecto 1

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include "head.php" ?>
+
+<div class="container">
+    <div class="col-md-12 row" id="resultados">
+         
+    </div>
+</div>
+<script src="js/main.js"></script>
+<?php include "fotter.php" ?>
